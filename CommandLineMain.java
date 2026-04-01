02e1760d8d129643a5de8e50211326044b1e20e0
usernamedoesntmatter <avtimurmain@gmail.com>
2026-04-02 01:48:20 +0300

    Style corrections

--- CommandLineMain.java
+++ CommandLineMain.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Objects;
 import java.util.Scanner;
 
 class CommandLineMain {
